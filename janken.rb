45eac65f614f892680ec995b193010d9e54a9e85
masaakira0629 <masaakira0629@yahoo.co.jp>
2019-01-14 20:04:57 +0900

    janken

---
+++ janken.rb
@@ -0,0 +1,25 @@
+class Game
+  def janken
+puts "じゃんけん・・・・"
+puts "[0]グー\n[1]チョキ\n[2]パー"
+
+player_hand = gets.to_i
+program_hand = rand(3)
+
+jankens = ["グー", "チョキ", "パー"]
+puts "あなたの手:#{jankens[player_hand]}, 相手の手:#{jankens[program_hand]}"
+
+ if player_hand == program_hand
+  puts "あいこ"
+  return true
+ elsif(player_hand == 0 && program_hand ==1)||(player_hand == 1 && program_hand == 2)
+  puts "あなたの勝ちです"
+  return false
+
+ else
+  puts "あなたの負けです"
+  return false
+ end
+
+ end
+end
